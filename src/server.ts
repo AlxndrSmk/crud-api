2476e69a214310d79caa7f91a92da581f2ba72c6
Alexander Samak <voprosy262@gmail.com>
2024-02-07 11:18:27 +0300

    feat: add get users by id and user create

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
-import { handleGetUsers } from './server/handlers/get';
+import { handleGetUsers, handleGetUserById } from './server/handlers/get';
 import sendResponse from './utils/response';
+import handleCreateUser from './server/handlers/post';
+
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   try {
     if (req.method === 'GET' && req.url === '/api/users') {
       handleGetUsers(req, res);
+    } else if (req.method === 'GET' && req.url?.startsWith('/api/users/')) {
+      handleGetUserById(req, res);
+    } else if (req.method === 'POST' && req.url === '/api/users') {
+      handleCreateUser(req, res);
     } else {
       sendResponse(res, 404, { message: 'Not found' });
     }
